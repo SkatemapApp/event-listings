3aa01572356b0637eeaf483d8b8779da72f2760a
swifthorseman <northislandking@gmail.com>
2017-03-24 21:20:20 +0000

    Add error handling

--- app.js
+++ app.js
@@ -6,6 +6,25 @@ var routes = require("./routes");
 
 app.use("/skatingEvents", routes);
 
+
+app.use(function(req, res, next) {
+  var err = new Error("Not found");
+  err.status = 404;
+  next(err);
+});
+
+// Error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
+});
+
+
+
 var port = process.env.PORT || 6633;
 
 app.listen(port, function() {
