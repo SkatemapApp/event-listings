7ce6dff201dc88662667c32c13e071a6eca36fa0
swifthorseman <northislandking@gmail.com>
2017-03-25 00:21:19 +0000

    Add Mongoose

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ var express = require("express");
 var app = express();
 var routes = require("./routes");
 
+
+var mongoose = require("mongoose");
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/event-listings')
+
+var db = mongoose.connection;
+db.on("error", function(err) {
+  console.error("connection error:", err);
+});
+
+db.once("open", function() {
+  console.log("db connection successful");
+});
+
+
 app.use("/skatingEvents", routes);
 
 
