86e404ef1a020151aa96aef9ef5530a3849cb252
swifthorseman <swifthorseman@users.noreply.github.com>
2017-11-23 00:31:53 +0000

    EL-16: Verify the URL returned in the header

--- test/submit_apiv1_test.js
+++ test/submit_apiv1_test.js
@@ -3,30 +3,32 @@ var expect = require('chai').expect;
 const should = require('should');
 var assert = require('assert');
 const request = require('supertest');
+var translateToModel = require("../model_adapter").translateToModel;
 
-describe('Routing', function() {
+describe('API', function() {
   var api_root_url = process.env.API_ROOT_URL || 'http://127.0.0.1:6633'
-  describe('API', function() {
+  describe('form submission via the POST request', function() {
+    var createdResourceUrl;
+    var formData = {
+      id: '20170507',
+      name: 'Sunday Stroll: For Wander-its-worth',
+      description: 'This Sunday our star quaddie lead marshal Kev will be taking the Sunday Stroll on a wonderful wander down to Wandsworth. Hopefully the temperatures and join us with music, wheels and a willingness to have a good time!!',
+      start: '2017-05-07 13:00:00 UTC',
+      meet: 'East end of Serpentine Road',
+      distance: 8,
+      status: 'Pending',
+      meet_lat: 51.504322,
+      meet_lon: 0.153358,
+      halftime: 'Wandsworth Bridge',
+      halftime_lat: 51.468164805146166,
+      halftime_lon: -0.19050121307373047,
+      marshal: 'Kev',
+      status_code: 1,
+      url: 'http://www.lfns.co.uk/for-wander-its-worth/',
+      url_route: 'http://www.lfns.co.uk/2017/05/07/route.xml'
+    }
     it('should correctly create a skating event', function(done) {
-			var formData = {
-				id: '20170507',
-			  name: 'Sunday Stroll: For Wander-its-worth',
- 				description: 'This Sunday our star quaddie lead marshal Kev will be taking the Sunday Stroll on a wonderful wander down to Wandsworth. Hopefully the temperatures and join us with music, wheels and a willingness to have a good time!!',
- 				start: '2017-05-07 13:00:00 UTC',
- 				meet: 'East end of Serpentine Road',
-				distance: 8,
-				status: 'Pending',
-				meet_lat: 51.504322,
-				meet_lon: 0.153358,
-				halftime: 'Wandsworth Bridge',
-				halftime_lat: 51.468164805146166,
-				halftime_lon: -0.19050121307373047,
-				marshal: 'Kev',
-				status_code: 1,
-				url: 'http://www.lfns.co.uk/for-wander-its-worth/',
-			  url_route: 'http://www.lfns.co.uk/2017/05/07/route.xml'
-			};
-
+;
 		request(api_root_url)
 			.post('/api/v1/submit')
       .type('form')
@@ -36,8 +38,9 @@ describe('Routing', function() {
 				if (err) {
 					throw err;
 				}
+        createdResourceUrl = res.header.location;
         const expectedUrlPattern = new RegExp('^' + api_root_url + '/api/v1' + '\.[0-9]' + '/skatingEvents/'+ '[0-9a-fA-F]+$');
-        expect(res.header.location).to.match(expectedUrlPattern);
+        expect(createdResourceUrl).to.match(expectedUrlPattern);
         expect(res.body).to.be.empty
 				done();
 			});
