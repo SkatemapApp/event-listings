736695ec73a5624dec4e490c02cce412a6d7c7bd
swifthorseman <swifthorseman@users.noreply.github.com>
2017-11-23 21:52:34 +0000

    Refactor test data #16

--- test/submit_apiv1_test.js
+++ test/submit_apiv1_test.js
@@ -4,29 +4,12 @@ const should = require('should');
 var assert = require('assert');
 const request = require('supertest');
 var translateToModel = require("../model_adapter").translateToModel;
+const formData = require("./data/form_submissions").sunday_stroll;
 
 describe('API', function() {
   var api_root_url = process.env.API_ROOT_URL || 'http://127.0.0.1:6633'
   describe('form submission via the POST request', function() {
     var createdResourceUrl;
-    var formData = {
-      id: '20170507',
-      name: 'Sunday Stroll: For Wander-its-worth',
-      description: 'This Sunday our star quaddie lead marshal Kev will be taking the Sunday Stroll on a wonderful wander down to Wandsworth. Hopefully the temperatures and join us with music, wheels and a willingness to have a good time!!',
-      start: '2017-05-07 13:00:00 UTC',
-      meet: 'East end of Serpentine Road',
-      distance: 8,
-      status: 'Pending',
-      meet_lat: 51.504322,
-      meet_lon: 0.153358,
-      halftime: 'Wandsworth Bridge',
-      halftime_lat: 51.468164805146166,
-      halftime_lon: -0.19050121307373047,
-      marshal: 'Kev',
-      status_code: 1,
-      url: 'http://www.lfns.co.uk/for-wander-its-worth/',
-      url_route: 'http://www.lfns.co.uk/2017/05/07/route.xml'
-    }
     it('should correctly create a skating event', function(done) {
 ;
 		request(api_root_url)
