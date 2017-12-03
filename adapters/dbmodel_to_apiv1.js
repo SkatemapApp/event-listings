34ebd929eb05a4220e3ae00d973f1063983a32e4
swifthorseman <swifthorseman@users.noreply.github.com>
2017-12-03 23:30:46 +0000

    Fix indentation using ESLint

--- adapters/dbmodel_to_apiv1.js
+++ adapters/dbmodel_to_apiv1.js
@@ -1,37 +1,37 @@
-"use strict";
+'use strict';
 
-const toUtcString = require("../utils").toUtc;
+const toUtcString = require('../utils').toUtc;
 
 function translateCoordinates(coordinates) {
-  if (coordinates.latitude === undefined
+    if (coordinates.latitude === undefined
       || coordinates.longitude === undefined) {
-    return [];
-  } else {
-    return [coordinates.latitude, coordinates.longitude];
-  }
+        return [];
+    } else {
+        return [coordinates.latitude, coordinates.longitude];
+    }
 }
 
 function translate(skatingEvent) {
-  var apiModel = {};
-  var key = skatingEvent._id;
-  apiModel[key] = ({name: skatingEvent.title,
-                    description: skatingEvent.description,
-                    start: toUtcString(skatingEvent.startAt),
-                    meet: skatingEvent.meetingPoint['name'],
-                    meet_latlon: translateCoordinates(skatingEvent.meetingPoint['coordinates']),
-                    halftime: skatingEvent.halfTime['name'],
-                    halftime_latlon: translateCoordinates(skatingEvent.halfTime['coordinates']),
-                    distance: skatingEvent.distance,
-                    marshal: skatingEvent.leadMarshal,
-                    status: skatingEvent.status['text'],
-                    status_code: skatingEvent.status['code'],
-                    url: skatingEvent.url,
-                    added: toUtcString(skatingEvent.createdAt),
-                    last_modified: toUtcString(skatingEvent.updatedAt),
-                    last_modified_route: toUtcString(skatingEvent.route['updatedAt']),
-                    route: []
-                    });
-  return apiModel;
+    var apiModel = {};
+    var key = skatingEvent._id;
+    apiModel[key] = ({name: skatingEvent.title,
+        description: skatingEvent.description,
+        start: toUtcString(skatingEvent.startAt),
+        meet: skatingEvent.meetingPoint['name'],
+        meet_latlon: translateCoordinates(skatingEvent.meetingPoint['coordinates']),
+        halftime: skatingEvent.halfTime['name'],
+        halftime_latlon: translateCoordinates(skatingEvent.halfTime['coordinates']),
+        distance: skatingEvent.distance,
+        marshal: skatingEvent.leadMarshal,
+        status: skatingEvent.status['text'],
+        status_code: skatingEvent.status['code'],
+        url: skatingEvent.url,
+        added: toUtcString(skatingEvent.createdAt),
+        last_modified: toUtcString(skatingEvent.updatedAt),
+        last_modified_route: toUtcString(skatingEvent.route['updatedAt']),
+        route: []
+    });
+    return apiModel;
 }
 
 function translateList(skatingEventModelList) {
@@ -50,5 +50,5 @@ function translateList(skatingEventModelList) {
     };
 }
 
-module.exports.translateToApiV1 = translate
-module.exports.translateToApiV1List = translateList
+module.exports.translateToApiV1 = translate;
+module.exports.translateToApiV1List = translateList;
--- adapters/form_to_dbmodel.js
+++ adapters/form_to_dbmodel.js
@@ -1,38 +1,38 @@
-var SkatingEvent = require("../models").SkatingEvent;
+var SkatingEvent = require('../models').SkatingEvent;
 
 function translateToModel(formData) {
-  var skatingEvent = new SkatingEvent(
-    {
-			"title": formData.name,
-			"description": formData.description,
-      "startAt": formData.start,
-      "meetingPoint": {
-        "name": formData.meet,
-        "coordinates": {
-          "latitude": formData.meet_lat,
-          "longitude": formData.meet_lon
+    var skatingEvent = new SkatingEvent(
+        {
+            'title': formData.name,
+            'description': formData.description,
+            'startAt': formData.start,
+            'meetingPoint': {
+                'name': formData.meet,
+                'coordinates': {
+                    'latitude': formData.meet_lat,
+                    'longitude': formData.meet_lon
+                }
+            },
+            'halfTime': {
+                'name': formData.halftime,
+                'coordinates': {
+                    'latitude': formData.halftime_lat,
+                    'longitude': formData.halftime_lon
+                }
+            },
+            'distance': formData.distance,
+            'leadMarshal': formData.marshal,
+            'status': {
+                'code': formData.status_code,
+                'text': formData.status
+            },
+            'url': formData.url,
+            'route': {
+                'url': formData.url_route
+            }
         }
-      },
-      "halfTime": {
-        "name": formData.halftime,
-        "coordinates": {
-          "latitude": formData.halftime_lat,
-          "longitude": formData.halftime_lon
-        }
-      },
-      "distance": formData.distance,
-      "leadMarshal": formData.marshal,
-      "status": {
-        "code": formData.status_code,
-        "text": formData.status
-      },
-      "url": formData.url,
-      "route": {
-        "url": formData.url_route
-      }
-    }
-  );
-  return skatingEvent;
+    );
+    return skatingEvent;
 }
 
 module.exports.translateToModel = translateToModel;
